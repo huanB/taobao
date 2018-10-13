209327459a3fc6d7cb20d37d86e33356a66680eb
huans <1838483620@qq.com>
2018-10-13 10:14:54 +0800

    未知

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp=require("gulp");
+//var sass=require("gulp-sass")
+
+gulp.task("copyHtml",function(){
+	gulp.src("*.html").pipe(gulp.dest("E:\\phpStudy\\WWW\\gitgulp"))
+});
+gulp.task("copyCss",function(){
+	gulp.src("css/**/*").pipe(gulp.dest("E:\\phpStudy\\WWW\\gitgulp\\css"))
+});
+gulp.task("copyJs",function(){
+	gulp.src("js/**/*").pipe(gulp.dest("E:\\phpStudy\\WWW\\gitgulp\\js"))
+});
+gulp.task("copyImg",function(){
+	gulp.src("img/*.{jpg,png}").pipe(gulp.dest("E:\\phpStudy\\WWW\\gitgulp\\img"))
+});
+gulp.task("copyFoot",function(){
+	gulp.src("foot/**/*").pipe(gulp.dest("E:\\phpStudy\\WWW\\gitgulp\\foot"))
+});
+//执行上面所有任务
+gulp.task("build",["copyHtml","copyCss","copyJs","copyImg","copyFoot"],function(){
+	console.log("ok");
+});
+
+
+
+
+gulp.task("watchall",function(){
+	gulp.watch("*.html",["copyHtml"]);
+	gulp.watch("css/**/*",["copyCss"]);
+	gulp.watch("js/**/*",["copyJs"]);
+	gulp.watch("img/*.{jpg,png}",["copyImg"]);
+	gulp.watch("foot/**/*",["copyFoot"]);
+});
